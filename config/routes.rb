c12d5bda0931b70b5f50c65970409445a59d9e34
Erina Ho <erina.hou@gmail.com>
2024-01-19 07:35:07 +1100

    Created a Yelp MVP app

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # get "restaurants", to: "restaurants#index"
+  # get "restaurants/new", to: "restaurants#new"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+
+gimlet = {name: "Gimlet", address: "33 Russell St, Melbourne VIC 3000", category: "french" }
+good_days = {name: "Good Days", address: "165 Sydney Rd, Brunswick VIC 3056", category: "chinese"}
+ima = {name: "Ima Asa Yoru", address: "1 Duckett St, Brunswick VIC 3056", category: "japanese" }
+bar_merenda = {name: "Bar Merenda", address: "Shop 3/22/24 Howe St, Daylesford VIC 3460", category: "italian" }
+etta = {name: "Etta", address: "60 Lygon St, Brunswick East VIC 3057,", category: "italian" }
+
+all_restaurants = [gimlet, good_days, ima, bar_merenda, etta]
+
+all_restaurants.each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+
+puts "Finished!"
